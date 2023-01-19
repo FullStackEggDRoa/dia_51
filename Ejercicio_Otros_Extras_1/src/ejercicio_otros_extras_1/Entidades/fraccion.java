7bd8bc93719143b6dee96d92a68ed7ae53749da8
Daniel Roa <droa@apgslt.com>
2023-01-19 09:21:52 -0500

    Ej_Otros_Extra_1 - Avance 1

---
+++ Ejercicio_Otros_Extras_1/src/ejercicio_otros_extras_1/Entidades/fraccion.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio_otros_extras_1.Entidades;
+
+/**
+ *
+ * @author droa
+ */
+public class fraccion {
+    // Atributos
+    private int denominador;
+    private int numerador;
+    
+    // Constructor
+
+    public fraccion() {
+    }
+
+    public fraccion(int denominador, int numerador) {
+        this.denominador = denominador;
+        this.numerador = numerador;
+    }
+    
+    
+}
---
+++ Ejercicio_Otros_Extras_1/src/ejercicio_otros_extras_1/Servicios/fraccionServicios.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio_otros_extras_1.Servicios;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author droa
+ */
+public class fraccionServicios {
+    Scanner leer = new Scanner(System.in);
+    
+}
